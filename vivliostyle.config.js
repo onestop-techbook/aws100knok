3931495b87b84609ef47d393370f8033356299d8
zzzzico12 <f505popj@yahoo.co.jp>
2025-04-30 14:33:10 +0900

    目次の更新漏れ

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -36,6 +36,7 @@ const def = {
     "chap-mob_engineer-aws-resilience-hub.md",
     "chap-iwamot-hiring-process.md",
     "chap-kazzpapa3-aws-cli.md",
+    "chap-otsuka-certification.md",
 
     // 後書き
     "90-postscript.md",
