d610eb3487aa9383107ca930a406d0c85b8cd4e8
takano-suzu69 <akiko.ishihara0131@gmail.com>
2025-05-03 02:31:39 +0900

    takano Organizations

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -42,6 +42,7 @@ const def = {
     "chap-mkdev10-amplify.md",
     "chap-mob_engineer-jaws-community-life.md",
 	"chap-hiyokoinfraengineer.md",
+    "chap-takano-org.md",
 
     // 後書き
     "90-postscript.md",
